81881bcaeb72c517da13697eeae18618e31fb667
manuel14mds <manuel14mds@gmail.com>
2024-06-20 15:04:30 -0500

    Organización de carpetas

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+/* import { InvitationGenerator } from './InvitationGenerator/InvitationGenerator'
+import { TimeConvecter } from './TimeConvecter/TimeConvecter'
+import { NotFound } from './NotFound/NotFound'
+
+export {
+    InvitationGenerator,
+    TimeConvecter,
+    NotFound,
+} */
