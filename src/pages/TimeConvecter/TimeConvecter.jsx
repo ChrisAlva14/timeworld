fb09a937c2b274e8a86d119b60b17903c70b7a65
mauriGaray <garaycarlosmauricio17@gmail.com>
2024-07-06 19:01:00 -0300

    timeCoverter_pseudoCodigo

--- src/pages/TimeConvecter/TimeConvecter.jsx
+++ src/pages/TimeConvecter/TimeConvecter.jsx
@@ -1,9 +1,13 @@
 function TimeConvecter() {
-    return (
-        <>
-            <h1>Pagina conversor horario</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Convertor horario</h1>
+      <h3>
+        Utiliza esta convertor para conocer el horario exacto de las ciudades
+        que desees
+      </h3>
+    </>
+  );
 }
 
-export default TimeConvecter
+export default TimeConvecter;
