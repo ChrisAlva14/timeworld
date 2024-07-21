dca199b7a78e684b9f861ba3d742cb1beafd69ff
Exequiel Schiavo <exelu1495@gmail.com>
2024-07-21 20:32:25 -0300

    feat: Completed layout and part of the functionality for the invitation generator page.

--- src/pages/InvitationGenerator/InvitationGenerator.jsx
+++ src/pages/InvitationGenerator/InvitationGenerator.jsx
@@ -1,145 +1,176 @@
 import { useState } from "react";
 import "./InvitationGenerator.css";
+import Invitation from "../Invitation/Invitation";
 
 function InvitationGenerator() {
   const [cities, setCities] = useState([]);
+
+  const [formData, setFormData] = useState({
+    titulo: "",
+    descripcion: "",
+    link: "",
+    fecha: "",
+    hora: "",
+    ciudadOrigen: "",
+    cities: [],
+  });
+
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [id]: value,
+    }));
+  };
 
   const handleCities = (event) => {
     const city = event.target.value;
     if (city && !cities.includes(city)) {
       setCities((prevCities) => {
         const updatedCities = [...prevCities, city];
-        console.log(updatedCities);
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          cities: updatedCities,
+        }));
         return updatedCities;
       });
     }
   };
 
   const handleRemove = (event) => {
-    const cityRemove = event.target.textContent.trim(); 
-
+    const cityRemove = event.target.textContent.trim();
     if (cities.includes(cityRemove)) {
-      const citiesRemoves = cities.filter((city) => city !== cityRemove);
-      setCities(citiesRemoves);
-      console.log(citiesRemoves);
+      const updatedCities = cities.filter((city) => city !== cityRemove);
+      setCities(updatedCities);
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        cities: updatedCities,
+      }));
     }
   };
 
   return (
     <main>
       <h1>Generar invitación </h1>
-      <p>
+      <p className="info">
         Utiliza este generador para{" "}
         <span>crear una invitación para tu reunión</span> con los horarios de
         las ciudades que desees
       </p>
 
-      <div className="info_container">
-        <div className="col">
-          <form>
-            <div className="div_input">
-              <label htmlFor="title">Título:</label>
-              <input
-                type="text"
-                id="titulo"
-                placeholder="Titulo de la invitación"
-              />
-            </div>
-
-            <div className="div_input">
-              <label htmlFor="message">Descripción:</label>
-              <textarea
-                name="message"
-                id="message"
-                placeholder="Descripción de la invitación"
-              ></textarea>
-            </div>
-
-            <div className="div_input">
-              <label htmlFor="link">Link:</label>
-              <input
-                type="text"
-                id="link"
-                placeholder="Link de la invitación"
-              />
-            </div>
-          </form>
-        </div>
-        <div className="col">
-          <form>
-            <div className="time">
+      <section id="container">
+        <div className="info_container">
+          <div className="col">
+            <form>
               <div className="div_input">
-                <label htmlFor="date">Fecha:</label>
-                <input type="date" id="date" />
+                <input
+                  type="text"
+                  id="titulo"
+                  placeholder="Titulo de la invitación"
+                  value={formData.titulo}
+                  onChange={handleInputChange}
+                />
               </div>
 
               <div className="div_input">
-                <label htmlFor="hour">Hora:</label>
-                <input type="time" id="hour" />
+                <textarea
+                  id="descripcion"
+                  placeholder="Descripción de la invitación"
+                  value={formData.descripcion}
+                  onChange={handleInputChange}
+                ></textarea>
               </div>
-            </div>
 
-            <div className="div_input">
-              <label htmlFor="cityOrigin">Ciudad de origen :</label>
-              <select name="cityOrigin" id="cityOrigin" onChange={handleCities}>
-                <option value="" disabled>
-                  --Seleccione la ciudad--
-                </option>
-                <option value="ARG">Argentina</option>
-                <option value="BRA">Brasil</option>
-                <option value="CAN">Canadá</option>
-                <option value="CHL">Chile</option>
-                <option value="COL">Colombia</option>
-                <option value="ESP">España</option>
-                <option value="MEX">México</option>
-                <option value="PER">Perú</option>
-                <option value="URU">Uruguay</option>
-                <option value="VEN">Venezuela</option>
-              </select>
-            </div>
-            <div className="div_input">
-              <label htmlFor="cityinvited">Ciudades invitadas:</label>
-              <select name="cityinvited" id="cityinvited">
-                <option value="" disabled>
-                  --Seleccione la ciudad--
-                </option>
-                <option value="ARG">Argentina</option>
-                <option value="BRA">Brasil</option>
-                <option value="CAN">Canadá</option>
-                <option value="CHL">Chile</option>
-                <option value="COL">Colombia</option>
-                <option value="ESP">España</option>
-                <option value="MEX">México</option>
-                <option value="PER">Perú</option>
-                <option value="URU">Uruguay</option>
-                <option value="VEN">Venezuela</option>
-              </select>
-            </div>
+              <div className="div_input">
+                <input
+                  type="text"
+                  id="link"
+                  placeholder="Link de la invitación"
+                  value={formData.link}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </form>
+          </div>
+          <div className="col">
+            <form>
+              <div className="time">
+                <div className="div_input">
+                  <input
+                    type="date"
+                    id="fecha"
+                    value={formData.fecha}
+                    onChange={handleInputChange}
+                  />
+                </div>
 
-            <div className="container_cities">
-              {cities.length > 0 ? (
-                cities.map((city, key) => {
-                  return (
-                    <p
-                      onClick={handleRemove}
-                      className="cityLabel"
-                      key={key}
-                    >
-                      {city}{" "}
-                    </p>
-                  );
-                })
-              ) : (
-                <p>No hay ciudades seleccionadas</p>
-              )}
-            </div>
-          </form>
+                <div className="div_input">
+                  <input
+                    type="time"
+                    id="hora"
+                    value={formData.hora}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+
+              <div className="div_input">
+                <select
+                  id="ciudadOrigen"
+                  value={formData.ciudadOrigen}
+                  onChange={handleInputChange}
+                >
+                  <option value="" disabled>
+                    --Seleccione la ciudad--
+                  </option>
+                  <option value="ARG">Argentina</option>
+                  <option value="BRA">Brasil</option>
+                  <option value="CAN">Canadá</option>
+                  <option value="CHL">Chile</option>
+                  <option value="COL">Colombia</option>
+                  <option value="ESP">España</option>
+                  <option value="MEX">México</option>
+                  <option value="PER">Perú</option>
+                  <option value="URU">Uruguay</option>
+                  <option value="VEN">Venezuela</option>
+                </select>
+              </div>
+              <div className="div_input">
+                <select id="cityinvited" onChange={handleCities}>
+                  <option value="" disabled>
+                    --Seleccione la ciudad--
+                  </option>
+                  <option value="ARG">Argentina</option>
+                  <option value="BRA">Brasil</option>
+                  <option value="CAN">Canadá</option>
+                  <option value="CHL">Chile</option>
+                  <option value="COL">Colombia</option>
+                  <option value="ESP">España</option>
+                  <option value="MEX">México</option>
+                  <option value="PER">Perú</option>
+                  <option value="URU">Uruguay</option>
+                  <option value="VEN">Venezuela</option>
+                </select>
+              </div>
+
+              <div className="container_cities">
+                {cities.length > 0 ? (
+                  cities.map((city, key) => {
+                    return (
+                      <p onClick={handleRemove} className="cityLabel" key={key}>
+                        {city}{" "}
+                      </p>
+                    );
+                  })
+                ) : (
+                  <p id="message">No hay ciudades seleccionadas</p>
+                )}
+              </div>
+            </form>
+          </div>
         </div>
-
-        <div className="container_button">
-          <button>Generar invitación</button>{" "}
-        </div>
-      </div>
+        {<Invitation formData={formData} />}
+      </section>
     </main>
   );
 }
