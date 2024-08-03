556e429458fd4d425b87f420f6da647f6fe7d974
Christopher Villalta <va23003@esfe.agape.edu.sv>
2024-08-02 22:38:31 -0600

    created a branch for working the 404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,24 @@
-function NotFound() {
-    return (
-        <div>
-            <h1>Pagina no encontrada</h1>
-        </div>
-    )
-}
+import './NotFound.css'
 
-export default NotFound
+import { useEffect, useState } from 'react';
+import './NotFound.css';
+function NotFound() {
+    const [oldClass] = useState(document.documentElement.className)
+
+    useEffect(() => {
+        document.documentElement.className = 'not-found';
+        console.log(oldClass);
+        return () => {
+            document.documentElement.className = oldClass;
+        };
+    }, [oldClass])
+    return (
+        <>
+            <h2 className='ubuntu-bold'>404</h2>
+            <p className='ubuntu-regular'>Ups! Parece que estás perdido...</p>
+            <br />
+            <p className='ubuntu-medium'>No pudimos encontrar la página que buscas {':('}</p>
+        </>
+    );
+}
+export default NotFound;
