b7ebc2725dbcb2dce83b4733ece4bf42b54c7209
manuel14mds <manuel14mds@gmail.com>
2024-08-28 19:36:32 -0500

    test: customize background image

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,71 +1,88 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Navbar } from "../Navbar/Navbar";
-import {
-  getLocation,
-  getWeather,
-  getBackgroundClass,
-} from "../../utils/weatherBg";
-//import { Footer } from "../Footer/Footer";
+import { getLocation, getWeather, getBackgroundClass} from "../../utils/weatherBg";
 import NavBotton from "../NavBotton/NavBotton";
 import styles from "./Layout.module.css"
+import { getReactiveBg, setReactiveBg } from "../../utils/localstorage.util";
 
 export default function Layout({ children }) {
-  const [weather, setWeather] = useState(null);
-  const [partOfDay, setPartOfDay] = useState(null);
-  const [error, setError] = useState(null);
-  const [loadingLocation, setLoadingLocation] = useState(true); // Estado para indicar si se está cargando la ubicación
-  const [loadingWeather, setLoadingWeather] = useState(true); // Estado para indicar si se está cargando el clima
-  const [coords, setCoords] = useState({ latitude: null, longitude: null });
-  useEffect(() => {
+    const [weather, setWeather] = useState(null);
+    const [partOfDay, setPartOfDay] = useState(null);
+    const [error, setError] = useState(null)
+    const [loadingLocation, setLoadingLocation] = useState(true); // Estado para indicar si se está cargando la ubicación
+    const [loadingWeather, setLoadingWeather] = useState(true); // Estado para indicar si se está cargando el clima
+    const [coords, setCoords] = useState({ latitude: null, longitude: null });
+    const [customExperience, setCustomExperience] = useState(() => getReactiveBg())// Estado para el background personalizado
+
+
+    const handleBtn = (value) => {
+        setReactiveBg(value)
+        setCustomExperience(value)
+    }
+
     // Obtener la posición actual del usuario si el navegador lo permite
-    getLocation(setCoords, setLoadingLocation, setError);
-    console.log("llamada a la api");
-  }, []);
+    const getDevicePosition = () => {
+        getLocation(setCoords, setLoadingLocation, setError);
+    }
 
-  useEffect(() => {
     // Obtener el clima cuando las coordenadas cambian
-    if (coords.latitude !== null && coords.longitude !== null) {
-      getWeather(
-        coords.latitude,
-        coords.longitude,
-        setPartOfDay,
-        setWeather,
-        setLoadingWeather,
-        setError
-      );
+    const getWeatherPosition = () => {
+        getWeather(
+            coords.latitude,
+            coords.longitude,
+            setPartOfDay,
+            setWeather,
+            setError,
+            setLoadingWeather,
+        );
     }
-  }, [coords]);
 
-  useEffect(() => {
     // Establecer la clase de fondo en el nivel del documento una vez que se tenga el clima
-    if (partOfDay && weather) {
-      const bgClass = `${partOfDay}-${getBackgroundClass(weather)}`;
-      document.documentElement.className = bgClass;
+    const updateBackground = () => {
+        if(error){
+            alert("No se ha podido actualizar el background")
+        }else{
+            const bgClass = `${partOfDay}-${getBackgroundClass(weather)}`;
+            document.documentElement.className = bgClass;
+        }
     }
-  }, [partOfDay, weather]);
 
-  // Renderizar contenido condicional basado en el estado de carga y errores
-  if (error) {
-    return <div className="error">{error}</div>;
-  }
 
-  if (loadingLocation || loadingWeather) {
-    return <div className="loading">Cargando...</div>;
-  }
-  return (
-    <>
-      <Navbar />
-      <main className={styles.main}>{children}</main>
-      <NavBotton />
-      {/* <footer className="footer">
-        footer -El clima actual es:{" "}
-        {`${partOfDay}-${getBackgroundClass(weather)}`}
-      </footer> */}
-    </>
-  );
+    useEffect(() => {
+        if (customExperience) {
+            getDevicePosition();
+        }
+    }, [customExperience]);
+    
+    useEffect(() => {
+        if (coords.latitude !== null && coords.longitude !== null) {
+            getWeatherPosition();
+        }
+    }, [coords]);
+    
+    useEffect(() => {
+        if (partOfDay && weather) {
+            updateBackground();
+        }else{
+            document.documentElement.className = 'dia-Sunny';
+        }
+    }, [partOfDay, weather]);
+
+    return (
+        <>
+            <Navbar />
+            <main className={styles.main}>{children}</main>
+            {customExperience ?
+                <button onClick={() => handleBtn(false)}>desactivar</button>
+                :
+                <button onClick={() => handleBtn(true)}>activar</button>
+            }
+            <NavBotton />
+        </>
+    );
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
