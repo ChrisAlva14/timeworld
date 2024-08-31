deb2da9b33442387a01f7a5646f460b42a01e2ed
githubfacu <elorzfacundo@gmail.com>
2024-08-31 09:25:51 -0300

    Se agrega la maquetacion de lla pagina resultados de conversion, + los componentes selectores en timeConvecter

---
+++ src/components/ConversionResults/DateCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './DateCard.module.css'
+
+export const DateCard = ({ciudad, pais, hora, fecha}) => {
+
+
+  return (
+    <article className={styles.container}>
+
+        <div className={styles.citiesData}>
+            <span>{ciudad}</span>
+            <span>{pais}</span>
+        </div>
+
+        <div className={styles.timeData}>
+            <span className={styles.hora}>{hora}</span>
+            <span className={styles.fecha}>{fecha}</span>
+        </div>
+
+    </article>
+  )
+}
